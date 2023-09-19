876b9d053cdd1f6341997fd9bfa79a586a44eb64
Adrien <adriengonz@outlook.fr>
2023-09-19 16:57:49 +0200

    Ajout de potion.go, mais non fonctionnel

--- src/potion.go
+++ src/potion.go
@@ -1,17 +1,24 @@
 package main
-/*
-import(
+
+import (
 	"fmt"
 )
 
-func TakePot() {
-	index := 0
-	for i := 0; range perso1.inventory {
-		if tab[i] == "potion de soin" {
+func TakePot(perso *Character) {
+	var index int = 0
+	for i := 1; i < len(perso.inventory); i++ {
+		if perso.inventory[i-1] == "potion de soin" {
 			index = i
 		}
 	}
-	if index == nil {
-		fmt.Println()
+	if index == 0 {
+		fmt.Println("Vous n'avez aucune potion dans votre inventaire !")
+	} else {
+		///RemovePot(perso.inventory, index)
+		fmt.Println("tkt")
 	}
-}*/
+}
+
+func RemovePot(tab []string, s int) []int {
+    return append(tab[:s], tab[s+1:]...)
+}
