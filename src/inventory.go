44920236c8658d9444a275a5ca1b6eaf9190cbaa
Adrien <adriengonz@outlook.fr>
2023-09-19 11:07:07 +0200

    Modification de la boucle et ajout de 'appel takepot

--- src/inventory.go
+++ src/inventory.go
@@ -6,12 +6,16 @@ import (
 
 func AccessInventory() { // Function for access inventory content
 	fmt.Println(perso1.inventory)
-	fmt.Println("Tapez 0 pour revenir au menu précedent")
+	fmt.Println("Tapez 0 pour revenir au menu précedent, ou tapez 1 pour utiliser une potion de vie")
 	var userinput int
 	fmt.Scan(&userinput)
-	for userinput != 0 { // While userinput is not 0, repeat the user input
-		fmt.Println("Votre commande n'a pas été reconnue, tapez 0 pour revenir au menu précédent")
+	for userinput != 0 || userinput != 1 { // While userinput is not 0, repeat the user input
+		fmt.Println("Votre commande n'a pas été reconnue, tapez 0 pour revenir au menu précédent ou taper 1 pour utiliser une potion de vie")
 		fmt.Scan(&userinput)
 	}
-	Menu()
+	if userinput == 1 {
+		TakePot()
+	} else {
+		Menu()
+	}
 }
