df968853f48f550a12f007ff063085a9b66ecef2
Adrien <adriengonz@outlook.fr>
2023-09-19 12:10:50 +0200

    Suppression de la potion dans inventory

--- src/inventory.go
+++ src/inventory.go
@@ -6,16 +6,12 @@ import (
 
 func AccessInventory() { // Function for access inventory content
 	fmt.Println(perso1.inventory)
-	fmt.Println("Tapez 0 pour revenir au menu précedent, ou tapez 1 pour utiliser une potion de vie")
+	fmt.Println("Tapez 0 pour revenir au menu précedent")
 	var userinput int
 	fmt.Scan(&userinput)
-	for userinput != 0 || userinput != 1 { // While userinput is not 0, repeat the user input
-		fmt.Println("Votre commande n'a pas été reconnue, tapez 0 pour revenir au menu précédent ou taper 1 pour utiliser une potion de vie")
+	for userinput != 0 { // While userinput is not 0, repeat the user input
+		fmt.Println("Votre commande n'a pas été reconnue, tapez 0 pour revenir au menu précédent")
 		fmt.Scan(&userinput)
 	}
-	if userinput == 1 {
-		TakePot()
-	} else {
-		Menu()
-	}
+	Menu()
 }
