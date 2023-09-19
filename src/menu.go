82cda213a2441e3e07d31a93556df21077e3d0ad
Adrien <adriengonz@outlook.fr>
2023-09-19 11:26:52 +0200

    Ajout du takepot sur menu

--- src/menu.go
+++ src/menu.go
@@ -7,12 +7,14 @@ import (
 
 func Menu() {
 	menu := 0
-
+	fmt.Println("Menu:")
 	fmt.Println("1- Afficher les informations du personnage")
 	fmt.Println('\n')
 	fmt.Println("2- Accéder au contenu de l’inventaire")
 	fmt.Println('\n')
-	fmt.Println("3- Quitter")
+	fmt.Println("3- Utiliser une potion de soin")
+	fmt.Println('\n')
+	fmt.Println("4- Quitter")
 
 	switch menu {
 	case 1:
@@ -20,6 +22,8 @@ func Menu() {
 	case 2:
 		AccessInventory(perso1)
 	case 3:
+		TakePot()
+	case 4:
 		os.Exit(0)
 	}
 }
