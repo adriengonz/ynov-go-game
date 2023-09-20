0b36674cc91cdb81120e7b1487f19ee0e9e89426
beleo <leo.benazeth@ynov.com>
2023-09-20 14:36:04 +0200

    départ de l'étape 10 création des skills

--- src/main.go
+++ src/main.go
@@ -1,5 +1,13 @@
 package main
 
+type skill int
+
+const (
+    boule_de_feu skill = iota
+    éclaire_de_givre skill = iota
+    Projectil_des_arcanes skill = iota
+)
+
 type Character struct { // Structure of Character
 	name        string
 	race        string
@@ -9,17 +17,19 @@ type Character struct { // Structure of Character
 	currentlife int
 	arrows      int
 	inventory   []string
+	skill       []string
 }
 func Char() Character { // Function that create character with his specifications
 	var perso Character
 		perso.name = "Legolas"
 		perso.race = "elf"
-		perso.class = "archer"
+		perso.class = "mage"
 		perso.level = 1
 		perso.maxlife = 100
 		perso.currentlife = 50
 		perso.arrows = 50
 		perso.inventory = []string{"potion de soin"}
+		perso.skill = []string Character.skill
 	return perso
 }
 
