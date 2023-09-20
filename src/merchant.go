3734d64a6f7b21713a2d0c1e5f836fb45c122f8c
beleo <leo.benazeth@ynov.com>
2023-09-20 11:13:07 +0200

    possibilité d'acheter les potions de soin

--- src/merchant.go
+++ src/merchant.go
@@ -7,18 +7,22 @@ import (
 
 func Merchant(perso *Character) {
 	menuinput := 0
-	fmt.Println("Bienvenue aventurié")
-	fmt.Println("1- Vendre une potion de soin (gratuitement)")
-	fmt.Println("2- Vendre une potion de poison (gratuitement)")
-	fmt.Println("3- Vendre un livre de sort = boule de feu")
+	fmt.Println("Bienvenue aventurier")
+	fmt.Println("1- Acheter une potion de soin (gratuitement)")
+	fmt.Println("2- Acheter une potion de poison (gratuitement)")
+	fmt.Println("3- Acheter un livre de sort = boule de feu")
 	fmt.Println("4- Revenire dans le menu principale")
 	fmt.Scan(&menuinput)
 
 	switch menuinput { // Menu in switch case form, permit to execute functions
 	case 1:
+		AddInventory(perso, "potion de soin")
 	case 2:
+		//(perso, "potion de poison")
 	case 3:
+		//(perso, "Livre de sort: boule de feu")
 	case 4:
 		os.Exit(0)
 	}
+	fmt.Println("Potion de soin à bien était ajoutée à votre inventaire")
 }
