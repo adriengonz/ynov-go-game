26132cf61ce28a85e3bb46d1ddd33d8d9bbc9d27
beleo <leo.benazeth@ynov.com>
2023-09-20 10:05:54 +0200

    retiré tout les \n dans le fichier menu

--- src/menu.go
+++ src/menu.go
@@ -9,15 +9,10 @@ func Menu(persovar *Character) { // Function that prints the menu of game, who t
 	menuinput := 0
 	fmt.Println("Menu:")
 	fmt.Println("1- Afficher les informations du personnage")
-	fmt.Println('\n')
 	fmt.Println("2- Accéder au contenu de l’inventaire")
-	fmt.Println('\n')
 	fmt.Println("3- Utiliser une potion de soin")
-	fmt.Println('\n')
 	fmt.Println("4- Marchand")
-	fmt.Println('\n')
 	fmt.Println("5- Quitter")
-	fmt.Println('\n')
 	fmt.Println("Votre choix ?")
 	fmt.Scan(&menuinput)
 
