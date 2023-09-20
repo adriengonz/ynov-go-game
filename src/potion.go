0919955049676f3afd75ca9d25a7db6abc6b5d9a
Adrien <adriengonz@outlook.fr>
2023-09-20 09:24:54 +0200

    Création de la potion de soin (non fonctionnel, ligne 14)

--- src/potion.go
+++ src/potion.go
@@ -5,20 +5,17 @@ import (
 )
 
 func TakePot(perso *Character) {
-	var index int = 0
-	for i := 1; i < len(perso.inventory); i++ {
-		if perso.inventory[i-1] == "potion de soin" {
-			index = i
+	for _, item := range perso.inventory { // Add health to character and remove the potion from the inventory
+		if item == "potion de soin" {
+			perso.currentlife += 50
+            if perso.currentlife > perso.maxlife {
+                perso.currentlife = 100
+            }
+            perso.inventory = append(perso.inventory[:item], perso.inventory[item+1:]...)
+			fmt.Println("Vous avez utilisé une potion de soin, votre santé est mainteant de", perso.currentlife)
+			Menu(perso)
 		}
 	}
-	if index == 0 {
-		fmt.Println("Vous n'avez aucune potion dans votre inventaire !")
-	} else {
-		///RemovePot(perso.inventory, index)
-		fmt.Println("tkt")
-	}
-}
-
-func RemovePot(tab []string, s int) []int {
-    return append(tab[:s], tab[s+1:]...)
+	fmt.Println("Vous n'avez aucune potion dans votre inventaire !")
+	Menu(perso)
 }
