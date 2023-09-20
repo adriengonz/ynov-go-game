9d162188a8f38989a21eccca57e48d03815eddd9
beleo <leo.benazeth@ynov.com>
2023-09-20 09:50:22 +0200

    perso.inventory corrigé (ligne 14)

--- src/potion.go
+++ src/potion.go
@@ -5,13 +5,13 @@ import (
 )
 
 func TakePot(perso *Character) {
-	for _, item := range perso.inventory { // Add health to character and remove the potion from the inventory
+	for i, item := range perso.inventory { // Add health to character and remove the potion from the inventory
 		if item == "potion de soin" {
 			perso.currentlife += 50
             if perso.currentlife > perso.maxlife {
                 perso.currentlife = 100
             }
-            perso.inventory = append(perso.inventory[:item], perso.inventory[item+1:]...)
+            perso.inventory = append(perso.inventory[:i], perso.inventory[i+1:]...)
 			fmt.Println("Vous avez utilisé une potion de soin, votre santé est mainteant de", perso.currentlife)
 			Menu(perso)
 		}
