c7ab00053341b5d14809258c681b6e1ea6dd668c
beleo <leo.benazeth@ynov.com>
2023-09-25 14:31:04 +0200

    timer de 2s entre chaque attaque, timer de 2s avant le retour au menu et mettre les dégâts à 5 points de dégâts

--- src/trainingfight.go
+++ src/trainingfight.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"math/rand"
 	"time"
 )
 
@@ -12,22 +11,27 @@ func trainingFight(perso *Character, monster *Monster) {
 	fmt.Println("Bienvenue dans le combat d'entraînement!")
 
 	for perso.currentlife > 0 && monster.currentlife > 0 {
-		fmt.Println("Tour", turn)
+		fmt.Println("\nTour", turn)
 		fmt.Println("Joueur - Points de vie :", perso.currentlife)
 		fmt.Println("Monstre - Points de vie :", monster.currentlife)
+		if turn%3 == 1 {
+			monster.attackpoint = monster.attackpoint*2
+		}
 
-		playerAttack := rand.Intn(20) + 1 // Tour du joueur
+		playerAttack := 5 // Tour du joueur
 		fmt.Println("Vous attaquez le monstre et lui infligez", playerAttack, "points de dégâts!")
 		monster.currentlife -= playerAttack
+		time.Sleep(2 * time.Second)
 
 		if monster.currentlife <= 0 {
 			fmt.Println("Vous avez vaincu le monstre! Félicitations!")
 			break
 		}
 
-		monsterAttack := rand.Intn(15) + 1 // Tour du monstre
+		monsterAttack := 5 // Tour du monstre
 		fmt.Println("Le monstre vous attaque et vous inflige", monsterAttack, "points de dégâts!")
 		perso.currentlife -= monsterAttack
+		time.Sleep(2 * time.Second)
 
 		if perso.currentlife <= 0 {
 			fmt.Println("Le monstre vous a vaincu. Vous avez perdu le combat.")
@@ -38,4 +42,6 @@ func trainingFight(perso *Character, monster *Monster) {
 	}
 
 	fmt.Println("Fin du combat d'entraînement.")
+	time.Sleep(2 * time.Second)
+	Menu(perso, monster)
 }
