88047b11a270ab984c32e127b458dd4fbbc3f5d2
beleo <leo.benazeth@ynov.com>
2023-09-26 11:45:16 +0200

    choisir d'attaquer ou de choisir l'inventaire dans me fichier trainingfight.go

--- src/trainingfight.go
+++ src/trainingfight.go
@@ -7,8 +7,7 @@ import (
 
 func trainingFight(perso *Character, monster *Monster) {
 	turn := 1
-
-	fmt.Println("Bienvenue dans le combat d'entraînement!")
+	attackinput := 0
 	
 	for perso.currentlife > 0 && monster.currentlife > 0 {
 		if turn%3 == 0 {
@@ -18,10 +17,21 @@ func trainingFight(perso *Character, monster *Monster) {
 		fmt.Println("Joueur - Points de vie :", perso.currentlife)
 		fmt.Println("Monstre - Points de vie :", monster.currentlife)
 
+		fmt.Println("Bienvenue dans le combat d'entraînement!")
+		fmt.Println("Que voulez vous faire ?")
+		fmt.Println("1 - attaquer")
+		fmt.Println("2 - inventaire")
+		fmt.Scan(&attackinput)
+
+		switch attackinput {
+		case 1:
 		playerAttack := 5 // Tour du joueur
 		fmt.Println("Vous attaquez le monstre et lui infligez", playerAttack, "points de dégâts!")
 		monster.currentlife -= playerAttack
 		time.Sleep(2 * time.Second)
+		case 2:
+			AccessInventory(perso, monster)
+		}
 
 		if monster.currentlife <= 0 {
 			fmt.Println("Vous avez vaincu le monstre! Félicitations!")
