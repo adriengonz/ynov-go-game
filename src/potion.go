6912c6e7b3b813d171dd5d20adf1ae5b52faef6d
beleo <leo.benazeth@ynov.com>
2023-09-26 12:20:15 +0200

    création de TakePotFight et AccessInventoryFight

--- src/potion.go
+++ src/potion.go
@@ -32,3 +32,19 @@ func PoisonPot(perso *Character, monster *Monster) { // Function of poisonpot
 		fmt.Println("Vous n'avez aucune potion de poison dans votre inventaire !")
 	}
 }
+
+func TakePotFight(perso *Character, monster *Monster) { // Add health to character and remove the potion from the inventory
+	if RemoveInventory(perso, "potion de soin") {
+		perso.currentlife += 50
+		if perso.currentlife > perso.maxlife {
+			perso.currentlife = 100
+		}
+		fmt.Println("Vous avez utilisé une potion de soin, votre santé est mainteant de", perso.currentlife)
+		time.Sleep(2 * time.Second)
+		trainingFight(perso, monster)
+	} else { // If RemoveInventory return false (so character doesn't have potion in his inventory)
+		fmt.Println("Vous n'avez aucune potion dans votre inventaire !")
+		time.Sleep(2 * time.Second)
+		trainingFight(perso, monster)
+	}
+}
--- src/trainingfight.go
+++ src/trainingfight.go
@@ -17,14 +17,15 @@ func trainingFight(perso *Character, monster *Monster) {
 		fmt.Println("Monstre - Points de vie :", monster.currentlife)
 
 		fmt.Println("Bienvenue dans le combat d'entraînement!")
-		fmt.Println("Que voulez vous faire ?")
+
+		fmt.Println("Que voulez vous faire ?") // Tour du joueur
 		fmt.Println("1 - attaquer")
 		fmt.Println("2 - inventaire")
 		fmt.Scan(&attackinput)
 
 		switch attackinput {
 		case 1:
-		playerAttack := 5 // Tour du joueur
+		playerAttack := 5
 		fmt.Println("Vous attaquez le monstre et lui infligez", playerAttack, "points de dégâts!")
 		monster.currentlife -= playerAttack
 		time.Sleep(2 * time.Second)
@@ -57,4 +58,21 @@ func trainingFight(perso *Character, monster *Monster) {
 	fmt.Println("Fin du combat d'entraînement.")
 	time.Sleep(2 * time.Second)
 	Menu(perso, monster)
+}
+
+func AccessInventoryFight(perso *Character, monster *Monster) { // Function for access inventory content
+	menuinventoryfight := 0
+	fmt.Println("Voici votre inventaire")
+	fmt.Println(perso.inventory)
+	fmt.Println("Tapez 0 pour revenir au menu précedent")
+	fmt.Println("Tapez 1 pour prendre une potion de soin")
+
+	fmt.Scan(&menuinventoryfight)
+
+	switch menuinventoryfight { // Menu in switch case form, permit to execute functions
+	case 0:
+		trainingFight(perso, monster)
+	case 1:
+		TakePotFight(perso, monster)
+	}
 }
