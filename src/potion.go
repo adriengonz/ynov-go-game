d68f2a087c44f2b6c136a7200310cc9281d892f1
beleo <leo.benazeth@ynov.com>
2023-09-27 10:27:28 +0200

    ajout commentaire de potion

--- src/potion.go
+++ src/potion.go
@@ -5,7 +5,7 @@ import (
 	"time"
 )
 
-func TakePot(perso *Character, monster *Monster) { // Add health to character and remove the potion from the inventory
+func TakePot(perso *Character, monster *Monster) { // Rajoute les points de vie que la potion doit donner et supprime l'item
 	if RemoveInventory(perso, "potion de soin") {
 		perso.currentlife += 50
 		if perso.currentlife > perso.maxlife {
@@ -14,26 +14,26 @@ func TakePot(perso *Character, monster *Monster) { // Add health to character an
 		fmt.Println("Vous avez utilisé une potion de soin, votre santé est mainteant de", perso.currentlife)
 		time.Sleep(2 * time.Second)
 		Menu(perso, monster)
-	} else { // If RemoveInventory return false (so character doesn't have potion in his inventory)
+	} else { // Si RemoveInventory renvoie <false> alors ça veut dire qu'on a pas de potion de soin dans l'inventaire
 		fmt.Println("Vous n'avez aucune potion dans votre inventaire !")
 		time.Sleep(2 * time.Second)
 		Menu(perso, monster)
 	}
 }
 
-func PoisonPot(perso *Character, monster *Monster) { // Function of poisonpot
+func PoisonPot(perso *Character, monster *Monster) { // La fonction de la poition de poison
 	if RemoveInventory(perso, "potion de poison") {
 		for i:= 0 ; i < 3 ; i++ {
 			perso.currentlife -= 10
 			fmt.Println("Des degats ont été infligés ! L'ennemi est à", monster.currentlife, "de vie !")
 			time.Sleep(3 * time.Second)
 		}
-	} else { // If RemoveInventory return false (so character doesn't have potion in his inventory)
+	} else { // Si RemoveInventory renvoie <false> alors ça veut dire qu'on a pas de potion de poison dans l'inventaire
 		fmt.Println("Vous n'avez aucune potion de poison dans votre inventaire !")
 	}
 }
 
-func TakePotFight(perso *Character, monster *Monster) { // Add health to character and remove the potion from the inventory
+func TakePotFight(perso *Character, monster *Monster) { // Rajoute les points de vie que la potion doit donner et supprime l'item
 	if RemoveInventory(perso, "potion de soin") {
 		perso.currentlife += 50
 		if perso.currentlife > perso.maxlife {
@@ -42,7 +42,7 @@ func TakePotFight(perso *Character, monster *Monster) { // Add health to charact
 		fmt.Println("Vous avez utilisé une potion de soin, votre santé est mainteant de", perso.currentlife)
 		time.Sleep(2 * time.Second)
 		TrainingFight(perso, monster)
-	} else { // If RemoveInventory return false (so character doesn't have potion in his inventory)
+	} else { // Si RemoveInventory renvoie <false> alors ça veut dire qu'on a pas de potion de soin dans l'inventaire
 		fmt.Println("Vous n'avez aucune potion dans votre inventaire !")
 		time.Sleep(2 * time.Second)
 		TrainingFight(perso, monster)
